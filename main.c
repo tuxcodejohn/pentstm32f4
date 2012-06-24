d66f21fba3b963944d8d9cdd92d4eb5d929348e6
john stone <john@tuxcode.org>
2012-06-24 04:26:42 +0200

    gute nacht

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include "arm_math.h"
 #include "math.h"
 
-#include "platf_led.h"
-#include "platf_i2c3.h"
+#include "platf_usart1.h"
+/*#include "platf_led.h"*/
+/*#include "platf_i2c3.h"*/
+
 
 static __IO uint32_t TimingDelay;
 static __IO uint32_t tick;
@@ -22,36 +24,70 @@ void TimingDelay_Decrement(void)
 	tick++;
 }
 
-void main(void)
+void mainhead(void)
 {
 	RCC_ClocksTypeDef RCC_Clocks;
 	RCC_GetClocksFreq(&RCC_Clocks);
+	unsigned int langweil = 0 ;
 
 	/* SysTick end of count event each 0.1ms */
 	SysTick_Config(RCC_Clocks.HCLK_Frequency / 10000);
+}
 
-	initLEDs();
-	setLed((LED_NORTH_PIN | LED_EAST_PIN |
-		LED_SOUTH_PIN | LED_WEST_PIN), 0);
 
+
+static void debug_dump_i2c_v(void){
+
+	char buf[32];
+
+	sprintf(buf,"cr1:%x   cr2:%x\r\n",
+			(((I2C_TypeDef*)I2C3_BASE)->CR1),
+			(((I2C_TypeDef*)I2C3_BASE)->CR2));
+	USART_puts(buf);
+	sprintf(buf,"oar1:%x   oar2:%x\r\n",
+			(((I2C_TypeDef*)I2C3_BASE)->OAR1),
+			(((I2C_TypeDef*)I2C3_BASE)->OAR2));
+	USART_puts(buf);
+	sprintf(buf,"dr:%x   ccr:%x\r\n",
+			(((I2C_TypeDef*)I2C3_BASE)->DR),
+			(((I2C_TypeDef*)I2C3_BASE)->CCR));
+	USART_puts(buf);
+	sprintf(buf,"sr1:%x   sr2:%x\r\n",
+			(((I2C_TypeDef*)I2C3_BASE)->SR1),
+			(((I2C_TypeDef*)I2C3_BASE)->SR2));
+	USART_puts(buf);
+	sprintf(buf,"trise:%x   test:%x\r\n",
+			(((I2C_TypeDef*)I2C3_BASE)->TRISE),
+			(uint16_t)0xc3d2 );
+	USART_puts(buf);
+
+}
+
+
+void main(void)
+{ 
+	mainhead();
+	char buf[32];
+	init_USART1();
+
+	 
+	USART_puts("ttttttttttt\r\n\n\n\ngoing init i2c3: \r\n");
 	i2cinit();
-	Delay(2);
-	setLed(LED_NORTH_PIN, 1);
-	Delay(2);
-	I2C_GenerateSTART(I2C3, ENABLE);
-	Delay(2);
-	setLed(LED_SOUTH_PIN, 1);
-	while (I2C_CheckEvent(I2C3, I2C_EVENT_MASTER_MODE_SELECT) != SUCCESS) ;
 
-	setLed((LED_NORTH_PIN | LED_EAST_PIN | LED_SOUTH_PIN | LED_WEST_PIN),
-	       1);
+	Delay(1);
+	debug_dump_i2c_v();
+	
+	USART_puts(" gen start & wait for master mode:\r\n");
+	 I2C_GenerateSTART( I2C3, ENABLE );
 
-	for (;;) {
-		;
-	Delay(2);
+	 debug_dump_i2c_v();
+	 I2C_GenerateSTART( I2C3, ENABLE );
+  	while( I2C_CheckEvent( I2C3, I2C_EVENT_MASTER_MODE_SELECT ) != SUCCESS );
 
-	}
+	USART_puts("done!\r\n");
 
-	//never get here
-	return;
+
+	for(;;)
+		USART_puts("Endloop \r\n");
+
 }
--- platf_i2c3.c
+++ platf_i2c3.c
@@ -10,23 +10,7 @@ void i2cinit(void)
 	GPIO_InitTypeDef GPIO_InitStructure;
 	I2C_InitTypeDef I2C_InitStructure;
 
-	GPIO_StructInit( &GPIO_InitStructure );
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;  //SCL at PA8
-	GPIO_InitStructure.GPIO_Speed= GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
-	GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOA , &GPIO_InitStructure);
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;  //SDA at PC9
-	GPIO_Init(GPIOC , &GPIO_InitStructure);
-
-
-	GPIO_PinAFConfig(GPIOA, GPIO_Pin_8 , GPIO_AF_I2C3);  
-	GPIO_PinAFConfig(GPIOC, GPIO_Pin_9 , GPIO_AF_I2C3);  
-
-	/* Enable the I2C3 peripheral clock */
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C3, ENABLE);
-	RCC_AHB1PeriphClockCmd((RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOA), ENABLE);
 
 	/* I2C3 peripheral configuration */
 	I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
@@ -39,6 +23,35 @@ void i2cinit(void)
 	I2C_DeInit(I2C3);
 	I2C_Init(I2C3, &I2C_InitStructure);
 	I2C_Cmd(I2C3, ENABLE);  
+
+
+	/* Enable the I2C3 peripheral clock */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C3, ENABLE);
+	RCC_AHB1PeriphClockCmd((RCC_AHB1Periph_GPIOC | RCC_AHB1Periph_GPIOA), ENABLE);
+
+
+	GPIO_StructInit( &GPIO_InitStructure );
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;  //SCL at PA8
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+       /* GPIO_InitStructure.GPIO_Speed= GPIO_Speed_50MHz;*/
+	/*GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;*/
+	/*GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;*/
+	GPIO_Init(GPIOA , &GPIO_InitStructure);
+
+	GPIO_StructInit( &GPIO_InitStructure );
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;  //SDA at PC9
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+       /* GPIO_InitStructure.GPIO_Speed= GPIO_Speed_50MHz;*/
+	/*GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;*/
+	/*GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;*/
+	GPIO_Init(GPIOC , &GPIO_InitStructure);
+
+
+	GPIO_PinAFConfig(GPIOA, GPIO_PinSource8 , GPIO_AF_I2C3);  
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource9 , GPIO_AF_I2C3);  
+
+
 
 	return ;
 }
